8b4a44e15c7e9f315488f15b1be71099de58764b
Lu15Gustavo <102445868+Lu15Gustavo@users.noreply.github.com>
2022-04-14 21:11:52 -0300

    Add files via upload

---
+++ Teste.py
@@ -0,0 +1,16 @@
+
+x  = input("digite um nome: ")   #MUITO FACIL MANDA MAIS
+
+if  (x== "João"):
+    print ("o salário de João é 1000 Reais Mensais")
+
+
+if  (x== "Pedro"):
+    print  ("o salário de Pedro é 1000 Reais mensais")
+
+if  (x== "Lucas"):
+    print ("O salario de Lucas foi 3000 reais mensais") 
+       
+
+
+
